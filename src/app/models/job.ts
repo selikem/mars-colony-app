85e5e6734e5a4cbabce5fa0b8ffe753b86486325
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-05-30 08:57:23 -0700

    initial commit

---
+++ src/app/models/job.ts
@@ -0,0 +1,6 @@
+export interface Job {
+    name: string;
+    id: string;
+    description: string;
+}
+
---
+++ src/app/models/report.ts
@@ -0,0 +1,14 @@
+export class Report {
+    atype: string;
+    date: string;
+    action: string;
+    colonist_id: string;
+
+    constructor( atype, date, action, colonistId ) {
+        this.atype = atype;
+        this.date = date;
+        this.action = action;
+        this.colonist_id = colonistId;
+    }
+}
+
